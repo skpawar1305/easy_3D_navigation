1a0a083bfe58d0356ec5a1ccdf624ec8b2aa3111
Shubham <skpawar1305@gmail.com>
2024-08-12 09:21:48 +0530

    few improvements

--- plan_3d_path.py
+++ plan_3d_path.py
@@ -20,15 +20,32 @@ import open3d as o3d
 
 from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, Marker
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
+from geometry_msgs.msg import Quaternion, Vector3
+from std_msgs.msg import ColorRGBA
 
 INCREASED_MAP_RESOLUTION = 0.05
 SPEED = 0.4
 LOOKAHEAD_DISTANCE = 0.4
 TARGET_ERROR = 0.2
-EXPANSION_SIZE = 3
-Z_THRESHOLD = 0.23
-GRID_RESOLUTION = 0.05
-voxel_size = GRID_RESOLUTION
+Z_THRESHOLD = 0.2
+VOXEL_SIZE = 0.1
+ROBOT_RADIUS = 0.35
+
+IS_SIMULATION = False
+
+
+def world_to_grid(world_coords, min_bound, voxel_size):
+    return tuple(
+        int((world_coords[i] - min_bound[i]) / voxel_size)
+        for i in range(3)
+    )
+
+
+def grid_to_world(grid_coords, min_bound, voxel_size):
+    return tuple(
+        grid_coords[i] * voxel_size + min_bound[i]
+        for i in range(3)
+    )
 
 
 def yaw_from_quaternion(x, y, z, w):
@@ -86,26 +103,14 @@ def find_nearest_3d_point(x, y, z, array_3d):
 
 
 def astar(array, start, goal):
-    neighbors = [
-        (-1, -1, -1), (-1, -1, 0), (-1, -1, 1),
-        (-1, 0, -1),  (-1, 0, 0),  (-1, 0, 1),
-        (-1, 1, -1),  (-1, 1, 0),  (-1, 1, 1),
-        
-        (0, -1, -1),  (0, -1, 0),  (0, -1, 1),
-        (0, 0, -1),               (0, 0, 1),
-        (0, 1, -1),   (0, 1, 0),   (0, 1, 1),
-        
-        (1, -1, -1),  (1, -1, 0),  (1, -1, 1),
-        (1, 0, -1),   (1, 0, 0),   (1, 0, 1),
-        (1, 1, -1),   (1, 1, 0),   (1, 1, 1)
-    ]
+    neighbors = [(i, j, k) for i in range(-1, 2) for j in range(-1, 2) for k in range(-1, 2) if not (i == 0 and j == 0 and k == 0)]
 
     def heuristic(a, b):
         # Euclidean distance as heuristic
         return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2) ** 0.5
 
-    def is_within_z_constraint(current, neighbor, z_constraint=0.2):
-        z_constraint = int(z_constraint / voxel_size)
+    def is_within_z_constraint(current, neighbor):
+        z_constraint = int(Z_THRESHOLD / VOXEL_SIZE)
         return abs(current[2] - neighbor[2]) <= z_constraint
 
     def is_within_bounds(coord):
@@ -115,8 +120,8 @@ def astar(array, start, goal):
         return array[coord[0], coord[1], coord[2]] == 100
 
     def has_no_occupied_cells_above(coord, vertical_min=0.3, vertical_range=0.6):
-        z_min = coord[2] + int(vertical_min / voxel_size)  # Start checking
-        z_max = coord[2] + int(vertical_range / voxel_size)  # End checking within the vertical range
+        z_min = coord[2] + int(vertical_min / VOXEL_SIZE)  # Start checking
+        z_max = coord[2] + int(vertical_range / VOXEL_SIZE)  # End checking within the vertical range
 
         # Use list comprehension to create a list of coordinates to check
         coords_to_check = [(coord[0], coord[1], z) for z in range(z_min, z_max + 1) if is_within_bounds((coord[0], coord[1], z))]
@@ -126,9 +131,9 @@ def astar(array, start, goal):
 
     def is_cylinder_collision_free(coord, radius):
         # Convert radius and base offset from meters to voxel grid units
-        grid_radius = radius / voxel_size
-        grid_z_start = int(0.3 / voxel_size)
-        grid_z_end = int(0.6 / voxel_size)
+        grid_radius = radius / VOXEL_SIZE
+        grid_z_start = int(0.3 / VOXEL_SIZE)
+        grid_z_end = int(0.6 / VOXEL_SIZE)
 
         # Calculate the number of points to check around the circumference
         num_points = int(2 * math.pi * grid_radius)
@@ -186,7 +191,7 @@ def astar(array, start, goal):
             if not has_no_occupied_cells_above(neighbor):
                 continue
 
-            if not is_cylinder_collision_free(neighbor, 0.3):
+            if not is_cylinder_collision_free(neighbor, ROBOT_RADIUS):
                 continue
 
             tentative_g_score = g_score[current] + heuristic(current, neighbor)
@@ -210,38 +215,28 @@ def reconstruct_path(came_from, current):
 
 class PointCloudToGrid(Node):
     def __init__(self):
-        super().__init__('pointcloud_to_grid_node')
-
-        # Parameters
-        self.declare_parameter('cloud_in_topic', '/navigation/octomap_point_cloud_centers_filtered')
-        self.declare_parameter('cell_size', GRID_RESOLUTION)
-        self.declare_parameter('z_threshold', Z_THRESHOLD)
-        self.declare_parameter('verbose', False)
-
-        self.cloud_in_topic = self.get_parameter('cloud_in_topic').get_parameter_value().string_value
-        self.cell_size = self.get_parameter('cell_size').get_parameter_value().double_value
-        self.z_threshold = self.get_parameter('z_threshold').get_parameter_value().double_value
-        self.verbose = self.get_parameter('verbose').get_parameter_value().bool_value
+        super().__init__("pointcloud_to_grid_node")
 
         # Publisher and Subscriber
-        self.sub_pc2 = self.create_subscription(PointCloud2, self.cloud_in_topic, self.pointcloud2_callback, 1)
+        pointcloud_topic = "/octomap_point_cloud_centers" if IS_SIMULATION else "/navigation/octomap_point_cloud_centers_filtered"
+        self.sub_pc2 = self.create_subscription(PointCloud2, pointcloud_topic, self.pointcloud2_callback, 1)
 
         self.listen_to_pointcloud = True
-        self.create_timer(1, self.timer_cb)
+        self.create_timer(5, self.timer_cb)
 
         # Initialize the transform buffer and listener
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
         self.create_subscription(PoseWithCovarianceStamped, "/initialpose", self.pose_cb, 1)
-        self.path_publisher = self.create_publisher(Marker, 'path_marker', 1)
+        self.path_publisher = self.create_publisher(Marker, "path_marker", 1)
 
         self.create_timer(0.1, self.path_follower_timer_callback)
 
-        self.twist_publisher = self.create_publisher(Twist, '/cmd_vel', 1)
+        self.twist_publisher = self.create_publisher(Twist, "/cmd_vel", 1)
 
         self.in_motion = False
-        self.srv = self.create_service(SetBool, '/navigation/follow_path', self.set_in_motion_callback)
+        self.srv = self.create_service(SetBool, "/navigation/follow_path", self.set_in_motion_callback)
 
         self.server = InteractiveMarkerServer(self, "goal_marker")
         self.create_goal_marker(0.,0.,0.)
@@ -254,20 +249,22 @@ class PointCloudToGrid(Node):
 
     def path_follower_timer_callback(self):
         try:
-            transform = self.tf_buffer.lookup_transform("map", "body", rclpy.time.Time())
+            base_frame = "base_footprint" if IS_SIMULATION else "gpe"
+            transform = self.tf_buffer.lookup_transform("map", base_frame, rclpy.time.Time())
+            self.robot_foot_location = [transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z]
 
-            # Extract the robot's position and orientation in the "map" frame
-            self.x = transform.transform.translation.x
-            self.y = transform.transform.translation.y
+            # Extract the robot"s position and orientation in the "map" frame
+            x = transform.transform.translation.x
+            y = transform.transform.translation.y
             self.robot_yaw = yaw_from_quaternion(transform.transform.rotation.x,
                                                 transform.transform.rotation.y,
                                                 transform.transform.rotation.z,
                                                 transform.transform.rotation.w)
 
-            self.robot_position = [self.x, self.y]
+            self.robot_position = [x, y]
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as ex:
-            # self.get_logger().warn("Could not get transform from map to body: {}".format(ex))
-            return
+            # self.get_logger().warn("Transform not found")
+            pass
 
         if not self.in_motion:
             return
@@ -280,8 +277,8 @@ class PointCloudToGrid(Node):
             return
         
         linear_velocity, angular_velocity, self.pursuit_index = pure_pursuit(
-            self.x,
-            self.y,
+            x,
+            y,
             self.robot_yaw,
             self.current_path_world,
             self.pursuit_index,
@@ -289,7 +286,7 @@ class PointCloudToGrid(Node):
             LOOKAHEAD_DISTANCE
         )
 
-        if(abs(self.x - self.current_path_world[-1][0]) < TARGET_ERROR and abs(self.y - self.current_path_world[-1][1]) < TARGET_ERROR):
+        if(abs(x - self.current_path_world[-1][0]) < TARGET_ERROR and abs(y - self.current_path_world[-1][1]) < TARGET_ERROR):
             self.in_motion = False
             print("Target reached")
             linear_velocity = 0
@@ -311,77 +308,26 @@ class PointCloudToGrid(Node):
         int_marker.scale = 1.0
 
         # Create the marker (a simple cube here for visualization)
-        marker = Marker()
-        marker.type = Marker.CUBE
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
-        marker.color.r = 1.0
-        marker.color.g = 0.0
-        marker.color.b = 0.0
-        marker.color.a = 1.0
+        marker = Marker(type=Marker.CUBE, scale=Vector3(x=0.1, y=0.1, z=0.1), color=ColorRGBA(r=1.0, g=0.0, b=0.0, a=1.0))
 
         # Attach the marker to a control for visualization
-        control = InteractiveMarkerControl()
-        control.always_visible = True
-        control.markers.append(marker)
+        control = InteractiveMarkerControl(always_visible=True, markers=[marker])
         int_marker.controls.append(control)
 
-        # Add control for moving in x, y, z
-        control_x = InteractiveMarkerControl()
-        control_x.name = "move_x"
-        control_x.orientation.w = 1.
-        control_x.orientation.x = 1.
-        control_x.orientation.y = 0.
-        control_x.orientation.z = 0.
-        control_x.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
-        int_marker.controls.append(control_x)
-
-        control_y = InteractiveMarkerControl()
-        control_y.name = "move_y"
-        control_y.orientation.w = 1.
-        control_y.orientation.x = 0.
-        control_y.orientation.y = 1.
-        control_y.orientation.z = 0.
-        control_y.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
-        int_marker.controls.append(control_y)
-
-        control_z = InteractiveMarkerControl()
-        control_z.name = "move_z"
-        control_z.orientation.w = 1.
-        control_z.orientation.x = 0.
-        control_z.orientation.y = 0.
-        control_z.orientation.z = 1.
-        control_z.interaction_mode = InteractiveMarkerControl.MOVE_AXIS
-        int_marker.controls.append(control_z)
-
-        # Add control for rotating in x, y, z
-        control_rot_x = InteractiveMarkerControl()
-        control_rot_x.name = "rotate_x"
-        control_rot_x.orientation.w = 1.
-        control_rot_x.orientation.x = 1.
-        control_rot_x.orientation.y = 0.
-        control_rot_x.orientation.z = 0.
-        control_rot_x.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
-        int_marker.controls.append(control_rot_x)
-
-        control_rot_y = InteractiveMarkerControl()
-        control_rot_y.name = "rotate_y"
-        control_rot_y.orientation.w = 1.
-        control_rot_y.orientation.x = 0.
-        control_rot_y.orientation.y = 1.
-        control_rot_y.orientation.z = 0.
-        control_rot_y.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
-        int_marker.controls.append(control_rot_y)
-
-        control_rot_z = InteractiveMarkerControl()
-        control_rot_z.name = "rotate_z"
-        control_rot_z.orientation.w = 1.
-        control_rot_z.orientation.x = 0.
-        control_rot_z.orientation.y = 0.
-        control_rot_z.orientation.z = 1.
-        control_rot_z.interaction_mode = InteractiveMarkerControl.ROTATE_AXIS
-        int_marker.controls.append(control_rot_z)
+        # Add controls for moving and rotating in x, y, z
+        axes = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+        for axis in axes:
+            move_control = InteractiveMarkerControl(
+                name=f"move_{'xyz'[axes.index(axis)]}",
+                orientation=Quaternion(x=float(axis[0]), y=float(axis[1]), z=float(axis[2]), w=1.0),
+                interaction_mode=InteractiveMarkerControl.MOVE_AXIS
+            )
+            rotate_control = InteractiveMarkerControl(
+                name=f"rotate_{'xyz'[axes.index(axis)]}",
+                orientation=Quaternion(x=float(axis[0]), y=float(axis[1]), z=float(axis[2]), w=1.0),
+                interaction_mode=InteractiveMarkerControl.ROTATE_AXIS
+            )
+            int_marker.controls.extend([move_control, rotate_control])
 
         # Add the interactive marker to the server
         self.server.insert(int_marker, feedback_callback=self.process_feedback)
@@ -400,23 +346,15 @@ class PointCloudToGrid(Node):
         if not hasattr(self, "goal_position"):
             self.get_logger().warn(f"Set the goal position using /goal_marker/update in rviz2")
             return
-        goal_coordinates_raw = self.goal_position
-        start_coordinates_raw = self.robot_foot_location
 
-        # Offset coordinates by the minimum bounds of the point cloud
-        goal_coordinates = (
-            int((goal_coordinates_raw[0] - self.min_bound[0]) / GRID_RESOLUTION),
-            int((goal_coordinates_raw[1] - self.min_bound[1]) / GRID_RESOLUTION),
-            int((goal_coordinates_raw[2] - self.min_bound[2]) / GRID_RESOLUTION)
-        )
+        if not hasattr(self, "min_bound"):
+            self.get_logger().warn("No occupancy grid available")
+            return
 
-        self.x, self.y, self.z = [
-            int((start_coordinates_raw[0] - self.min_bound[0]) / GRID_RESOLUTION),
-            int((start_coordinates_raw[1] - self.min_bound[1]) / GRID_RESOLUTION),
-            int((start_coordinates_raw[2] - self.min_bound[2]) / GRID_RESOLUTION)
-        ]
+        goal_coordinates = world_to_grid(self.goal_position, self.min_bound, VOXEL_SIZE)
+        x, y, z = world_to_grid(self.robot_foot_location, self.min_bound, VOXEL_SIZE)
 
-        self.x, self.y, self.z = find_nearest_3d_point(self.x, self.y, self.z, self.occupancy_grid)
+        x, y, z = find_nearest_3d_point(x, y, z, self.occupancy_grid)
 
         # Check if the calculated indices are within the bounds of the occupancy grid
         if 0 <= goal_coordinates[0] < self.occupancy_grid.shape[0] and \
@@ -426,16 +364,16 @@ class PointCloudToGrid(Node):
         else:
             self.get_logger().warn(f"Goal coordinates {goal_coordinates} are out of bounds.")
 
-        if 0 <= self.x < self.occupancy_grid.shape[0] and \
-        0 <= self.y < self.occupancy_grid.shape[1] and \
-        0 <= self.z < self.occupancy_grid.shape[2]:
-            self.occupancy_grid[self.x, self.y, self.z] = 100
+        if 0 <= x < self.occupancy_grid.shape[0] and \
+        0 <= y < self.occupancy_grid.shape[1] and \
+        0 <= z < self.occupancy_grid.shape[2]:
+            self.occupancy_grid[x, y, z] = 100
         else:
-            self.get_logger().warn(f"Start coordinates ({self.x}, {self.y}, {self.z}) are out of bounds.")
+            self.get_logger().warn(f"Start coordinates ({x}, {y}, {z}) are out of bounds.")
             return
 
         path = astar(
-            self.occupancy_grid, (self.x, self.y, self.z),
+            self.occupancy_grid, (x, y, z),
             goal_coordinates
         )
 
@@ -460,8 +398,7 @@ class PointCloudToGrid(Node):
             pose = Pose()
 
             # Convert from grid coordinates (p[0], p[1]) to world coordinates
-            pose.position.x, pose.position.y, pose.position.z = (p[0])* GRID_RESOLUTION + self.min_bound[0], (p[1]) * GRID_RESOLUTION + self.min_bound[1], (p[2]) * GRID_RESOLUTION + self.min_bound[2]
-
+            pose.position.x, pose.position.y, pose.position.z = grid_to_world(p, self.min_bound, VOXEL_SIZE)
             path_marker.points.append(pose.position)
 
             self.current_path_world.append([pose.position.x, pose.position.y])
@@ -471,14 +408,6 @@ class PointCloudToGrid(Node):
 
     def timer_cb(self):
         self.listen_to_pointcloud = True
-
-        try:
-            # transform = self.tf_buffer.lookup_transform('map', 'base_footprint', rclpy.time.Time())
-            transform = self.tf_buffer.lookup_transform('map', 'gpe', rclpy.time.Time())
-            self.robot_foot_location = [transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z]
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as ex:
-            # self.get_logger().warn('Transform not found')
-            pass
 
     def pointcloud2_callback(self, msg):
         if not self.listen_to_pointcloud:
@@ -490,11 +419,11 @@ class PointCloudToGrid(Node):
             points = np.array([[float(p[0]), float(p[1]), float(p[2])] for p in point_cloud2.read_points(msg, skip_nans=True)])
 
             if points.size == 0:
-                self.get_logger().info('No points received in the point cloud')
+                self.get_logger().info("No points received in the point cloud")
                 return
 
             if points.ndim != 2 or points.shape[1] != 3:
-                self.get_logger().error(f'Unexpected points shape: {points.shape}')
+                self.get_logger().error(f"Unexpected points shape: {points.shape}")
                 return
 
             # Convert numpy array to Open3D PointCloud
@@ -502,17 +431,16 @@ class PointCloudToGrid(Node):
             pcd.points = o3d.utility.Vector3dVector(points)
 
             # Create a voxel grid from the point cloud
-            voxel_size = self.cell_size  # Use the cell size as the voxel size
-            voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size)
+            voxel_grid = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, VOXEL_SIZE)
 
             # Determine grid dimensions based on voxel grid bounds
             min_bound = voxel_grid.get_min_bound()
             max_bound = voxel_grid.get_max_bound()
             self.min_bound = deepcopy(min_bound)
             
-            grid_size_x = int((max_bound[0] - min_bound[0]) / voxel_size) + 1
-            grid_size_y = int((max_bound[1] - min_bound[1]) / voxel_size) + 1
-            grid_size_z = int((max_bound[2] - min_bound[2]) / voxel_size) + 1
+            grid_size_x = int((max_bound[0] - min_bound[0]) / VOXEL_SIZE) + 1
+            grid_size_y = int((max_bound[1] - min_bound[1]) / VOXEL_SIZE) + 1
+            grid_size_z = int((max_bound[2] - min_bound[2]) / VOXEL_SIZE) + 1
 
             # Initialize 3D occupancy grid with -1 (unknown)
             occupancy_grid = np.full((grid_size_x, grid_size_y, grid_size_z), -1, dtype=int)
@@ -530,7 +458,7 @@ class PointCloudToGrid(Node):
             self.occupancy_grid = deepcopy(occupancy_grid)
 
         except Exception as e:
-            self.get_logger().error(f'Error processing point cloud: {e}')
+            self.get_logger().error(f"Error processing point cloud: {e}")
 
 
 def main(args=None):
@@ -541,5 +469,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
